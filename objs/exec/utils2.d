d6ba5d8991275f4d92f6d7a2a2db0d1fa22d2717
Daria SYDELNYKOVA <dsydelny@made-f0Cr10s5.clusters.42paris.fr>
2023-10-01 01:09:51 +0200

    lol

--- objs/exec/utils2.d
+++
@@ -1,8 +0,0 @@
-objs/exec/utils2.o: srcs/exec/utils2.c includes/minishell.h \
-  includes/../libft/libft.h libft/get_next_line.h
-
-includes/minishell.h:
-
-includes/../libft/libft.h:
-
-libft/get_next_line.h:
--- objs/parsing/ft_signals.d
+++
@@ -1,8 +0,0 @@
-objs/parsing/ft_signals.o: srcs/parsing/ft_signals.c includes/minishell.h \
-  includes/../libft/libft.h libft/get_next_line.h
-
-includes/minishell.h:
-
-includes/../libft/libft.h:
-
-libft/get_next_line.h:
